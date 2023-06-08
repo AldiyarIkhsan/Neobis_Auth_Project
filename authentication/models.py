bf5310654b824eda9e326895f0993bdda3638418
AldiyarIkhsan <usoul31@gmail.com>
2023-06-08 16:59:00 +0500

    changed registration view (deleted password)

--- authentication/models.py
+++ authentication/models.py
@@ -8,16 +8,17 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if email is None:
             raise TypeError('Users should have a Email')
+
         user = self.model(email=self.normalize_email(email))
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, username, email, password=None):
+    def create_superuser(self, email, password=None):
         if password is None:
             raise TypeError('Password should not be none')
 
-        user = self.create_user(username, email, password)
+        user = self.create_user(email, password)
         user.is_superuser = True
         user.is_staff = True
         user.save()
@@ -37,7 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     updated_at = models.DateField(auto_now=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,11 +9,9 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(max_length=15, min_length=8, write_only=True)
-
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email']
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
